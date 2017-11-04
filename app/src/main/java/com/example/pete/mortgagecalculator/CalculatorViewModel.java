c6a833f17b3a1004d634b4e1594272fa2ec7e904
Pete <PeteGrapentien@gmail.com>
2017-11-04 15:05:37 -0400

    Added mortgage calculator functionality

--- app/src/main/java/com/example/pete/mortgagecalculator/CalculatorViewModel.java
+++ app/src/main/java/com/example/pete/mortgagecalculator/CalculatorViewModel.java
@@ -1,5 +1,7 @@
 package com.example.pete.mortgagecalculator;
 
+import java.text.NumberFormat;
+
 /**
  * Created by pete on 11/4/17.
  */
@@ -7,6 +9,15 @@ package com.example.pete.mortgagecalculator;
 public class CalculatorViewModel {
 
     public String calculateMortgage(double amount, double interest, double duration) {
-        return "1.0";
+            interest /= 100.0;
+            double monthlyRate = interest / 12.0;
+            double termInMonths = duration * 12;
+            double monthlyPayment =
+                    (amount*monthlyRate) /
+                            (1-Math.pow(1+monthlyRate, -termInMonths));
+
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+        return formatter.format(monthlyPayment).toString();
     }
 }
