28feac59ddf01ed4869fb55c1ac8d9e93bbe4779
Pete <PeteGrapentien@gmail.com>
2017-11-04 13:27:11 -0400

    Finished basic text entry

--- app/src/main/java/com/example/pete/mortgagecalculator/CalculatorActivity.java
+++ app/src/main/java/com/example/pete/mortgagecalculator/CalculatorActivity.java
@@ -2,12 +2,26 @@ package com.example.pete.mortgagecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    CalculatorViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        EditText amount = (EditText) findViewById(R.id.text_amount);
+        EditText interest = (EditText) findViewById(R.id.text_interest);
+        EditText duration = (EditText) findViewById(R.id.text_duration);
+
+        Button calculateButton = (Button) findViewById(R.id.calculate_button);
+
+        viewModel = new CalculatorViewModel();
+
     }
 }
---
+++ app/src/main/java/com/example/pete/mortgagecalculator/CalculatorViewModel.java
@@ -0,0 +1,12 @@
+package com.example.pete.mortgagecalculator;
+
+/**
+ * Created by pete on 11/4/17.
+ */
+
+public class CalculatorViewModel {
+
+    public void calculateMortgage(float amount, float interest, float duration) {
+
+    }
+}
