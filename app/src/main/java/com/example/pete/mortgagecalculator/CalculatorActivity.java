9946e679a4507bac8054382a370c62ec3889a411
Pete <PeteGrapentien@gmail.com>
2017-11-11 15:40:52 -0500

    Redid the dollar sign stuff

--- app/src/main/java/com/example/pete/mortgagecalculator/CalculatorActivity.java
+++ app/src/main/java/com/example/pete/mortgagecalculator/CalculatorActivity.java
@@ -1,18 +1,11 @@
 package com.example.pete.mortgagecalculator;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.text.NumberFormat;
 
